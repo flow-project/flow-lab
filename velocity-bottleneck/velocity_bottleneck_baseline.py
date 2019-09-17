a6d040648d0693edbb18f80aecc5431870a85bd9
gilbertbahati <bahati95@berkeley.edu>
2019-09-17 00:20:48 -0700

    started generatiting baseline results

--- velocity-bottleneck/velocity_bottleneck_baseline.py
+++ velocity-bottleneck/velocity_bottleneck_baseline.py
@@ -1,89 +1,229 @@
-from flow.networks.bottleneck import BottleneckNetwork, ADDITIONAL_NET_PARAMS
-from flow.core.params import VehicleParams, InFlows, SumoCarFollowingParams, SumoLaneChangeParams
-from flow.controllers.car_following_models import IDMController
-from flow.controllers import SimLaneChangeController, ContinuousRouter, RLController
-from flow.core.params import NetParams
-from flow.core.params import InitialConfig
+"""File demonstrating formation of congestion in bottleneck."""
+from flow.core.params import SumoParams, EnvParams, NetParams, InitialConfig, \
+    InFlows, SumoLaneChangeParams, SumoCarFollowingParams
+from flow.core.params import VehicleParams
 from flow.core.params import TrafficLightParams
-from flow.core.params import SumoParams
-from flow.core.params import EnvParams
+
+from flow.networks.bottleneck import BottleneckNetwork
+from flow.controllers import SimLaneChangeController, ContinuousRouter
 from flow.envs.bottleneck import BottleneckEnv
-from flow.envs.bottleneck import ADDITIONAL_ENV_PARAMS
 from flow.core.experiment import Experiment
+from flow.core.util import emission_to_csv
+import matplotlib as plt
 
-#wrap in function
-"""This network, as well as all other networks in Flow, is parametrized by the following arguments:
-name _/
-vehicles_/
-net_params_/
-initial_config_/
-traffic_lights_/"""
 
-name = "velocity_bottleneck_baseline"
-vehicles = VehicleParams()
+import logging
 
-#specify your controllers
-vehicles.add("human",
-             acceleration_controller= (IDMController, {}),
-             car_following_params=SumoCarFollowingParams(speed_mode="obey_safe_speed"),
-             # lane_change_controller= (SimLaneChangeController, {}),
-             num_vehicles = 20)
-#test adding an rl vehicle
-vehicles.add("rl",
-             acceleration_controller= (RLController, {}),
-             car_following_params=SumoCarFollowingParams(speed_mode="obey_safe_speed"),
-             # lane_change_controller= (SimLaneChangeController, {}),
-             num_vehicles = 3)
+import numpy as np
+import os
+import pandas as pd
+SCALING = 1
+DISABLE_TB = True
 
-inflow = InFlows()
-inflow.add(veh_type="human",
-           edge="1",
-           depart_lane="random",
-           vehs_per_hour=2300)
-inflow.add(veh_type="rl",
-           edge="1",
-           depart_lane="random",
-           vehs_per_hour=400)
-# inflow.add(
-#     veh_type="human",
-#     edge="1",
-#     vehsPerHour=2300,
-#     departLane="random",
-#     departSpeed=10)
-# print(ADDITIONAL_NET_PARAMS)
-#we use the default {'scaling': 1, 'speed_limit': 23}
+# If set to False, ALINEA will control the ramp meter
+DISABLE_RAMP_METER = True
+INFLOW = 2300
 
-#specify network parameters
-net_params = NetParams(inflows = inflow, additional_params=ADDITIONAL_NET_PARAMS)
 
-#what's happening here? check
-initial_config = InitialConfig(spacing="random",
-                               shuffle=True,
-                        min_gap=5,
-                        # lanes_distribution=float("inf"),
-                        edges_distribution=["2", "3", "4", "5"]) #need to figure these parameters out
+class BottleneckDensityExperiment(Experiment):
+    """Experiment object for bottleneck-specific simulations.
 
-traffic_lights = TrafficLightParams()
+    Extends flow.core.experiment.Experiment
+    """
 
-#Create an nvironment
-# EnvParams
-# SumoParams _/
-# Network_/
-sumo_params = SumoParams(sim_step=0.1, render=True, overtake_right=False, restart_instance=False) #extra parameters
-env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
+    def __init__(self, env):
+        """Instantiate the bottleneck experiment."""
+        super().__init__(env)
 
-#scenario/network takes inputs object 1
-network = BottleneckNetwork(name=name,
-                      vehicles=vehicles,
-                      net_params=net_params,
-                      initial_config=initial_config,
-                      traffic_lights=traffic_lights)
+    def run(self, num_runs, num_steps, rl_actions=None, convert_to_csv=False):
+        """See parent class."""
+        info_dict = {}
+        if rl_actions is None:
 
-#object 2
-env = BottleneckEnv(env_params, sumo_params, network)
+            def rl_actions(*_):
+                return None
 
-#object 3
-exp = Experiment(env)
+        rets = []
+        mean_rets = []
+        ret_lists = []
+        vels = []
+        mean_vels = []
+        std_vels = []
+        mean_densities = []
+        mean_outflows = []
+        for i in range(num_runs):
+            vel = np.zeros(num_steps)
+            logging.info('Iter #' + str(i))
+            ret = 0
+            ret_list = []
+            step_outflows = []
+            step_densities = []
+            state = self.env.reset()
+            for j in range(num_steps):
+                state, reward, done, _ = self.env.step(rl_actions(state))
+                vel[j] = np.mean(self.env.k.vehicle.get_speed(
+                    self.env.k.vehicle.get_ids()))
+                ret += reward
+                ret_list.append(reward)
 
-#run
-exp.run(1, 10000)
+                env = self.env
+                step_outflow = env.k.vehicle.get_outflow_rate(20)
+                density = self.env.get_bottleneck_density()
+
+                step_outflows.append(step_outflow)
+                step_densities.append(density)
+                if done:
+                    break
+            rets.append(ret)
+            vels.append(vel)
+            # mean_densities.append(sum(step_densities[100:]) /
+            #                       (num_steps - 100))
+            env = self.env
+            outflow = env.k.vehicle.get_outflow_rate(10000)
+            mean_outflows.append(outflow)
+            mean_rets.append(np.mean(ret_list))
+            ret_lists.append(ret_list)
+            mean_vels.append(np.mean(vel))
+            std_vels.append(np.std(vel))
+            print('Round {0}, return: {1}'.format(i, ret))
+
+        info_dict['returns'] = rets
+        info_dict['velocities'] = vels
+        info_dict['mean_returns'] = mean_rets
+        info_dict['per_step_returns'] = ret_lists
+        info_dict['average_outflow'] = np.mean(mean_outflows)
+        info_dict['per_rollout_outflows'] = mean_outflows
+
+        # info_dict['average_rollout_density_outflow'] = np.mean(mean_densities)
+
+        # print('Average, std return: {}, {}'.format(
+        #     np.mean(rets), np.std(rets)))
+        # print('Average, std speed: {}, {}'.format(
+        #     np.mean(mean_vels), np.std(std_vels)))
+        self.env.terminate()
+
+        return info_dict
+
+
+def bottleneck_example(flow_rate, horizon, restart_instance=False,
+                       render=None):
+    """
+    Perform a simulation of vehicles on a bottleneck.
+
+    Parameters
+    ----------
+    flow_rate : float
+        total inflow rate of vehicles into the bottleneck
+    horizon : int
+        time horizon
+    restart_instance: bool, optional
+        whether to restart the instance upon reset
+    render: bool, optional
+        specifies whether to use the gui during execution
+
+    Returns
+    -------
+    exp: flow.core.experiment.Experiment
+        A non-rl experiment demonstrating the performance of human-driven
+        vehicles on a bottleneck.
+    """
+    if render is None:
+        render = False
+
+    sim_params = SumoParams(
+        sim_step=0.5,
+        render=render,
+        overtake_right=False,
+        restart_instance=restart_instance,
+        emission_path='data')
+
+    vehicles = VehicleParams()
+
+    vehicles.add(
+        veh_id="human",
+        lane_change_controller=(SimLaneChangeController, {}),
+        routing_controller=(ContinuousRouter, {}),
+        car_following_params=SumoCarFollowingParams(
+            speed_mode=25,
+        ),
+        lane_change_params=SumoLaneChangeParams(
+            lane_change_mode=1621,
+        ),
+        num_vehicles=1)
+    # print(ADDITIONAL_ENV_PARAMS)
+    # {'max_accel': 3, 'max_decel': 3, 'lane_change_duration': 5, 'disable_tb': True, 'disable_ramp_metering': True}
+    additional_env_params = {
+        "target_velocity": 40,
+        "max_accel": 1,
+        "max_decel": 1,
+        "lane_change_duration": 5,
+        "add_rl_if_exit": False,
+        "disable_tb": DISABLE_TB,
+        "disable_ramp_metering": DISABLE_RAMP_METER
+    }
+    env_params = EnvParams(
+        horizon=horizon, additional_params=additional_env_params)
+
+    inflow = InFlows()
+    inflow.add(
+        veh_type="human",
+        edge="1",
+        vehsPerHour=flow_rate,
+        departLane="random",
+        departSpeed=10)
+
+    traffic_lights = TrafficLightParams()
+    if not DISABLE_TB:
+        traffic_lights.add(node_id="2")
+    if not DISABLE_RAMP_METER:
+        traffic_lights.add(node_id="3")
+
+    # print(ADDITIONAL_NET_PARAMS)
+    # we use the default {'scaling': 1, 'speed_limit': 23}
+    additional_net_params = {"scaling": SCALING, "speed_limit": 23}
+    net_params = NetParams(
+        inflows=inflow,
+        additional_params=additional_net_params)
+
+    initial_config = InitialConfig(
+        spacing="random",
+        min_gap=5,
+        lanes_distribution=float("inf"),
+        edges_distribution=["2", "3", "4", "5"])
+
+    network = BottleneckNetwork(
+        name="bay_bridge_toll",
+        vehicles=vehicles,
+        net_params=net_params,
+        initial_config=initial_config,
+        traffic_lights=traffic_lights)
+
+    env = BottleneckEnv(env_params, sim_params, network)
+
+    return BottleneckDensityExperiment(env)
+
+if __name__ == '__main__':
+    # import the experiment variable
+    # inflow, number of steps, binary
+
+    # (from paper)
+    # To compute this, we swept
+    # over inflows from 400 to 2500 in steps of 100, ran 10 runs
+    # for each inflow value, and stored the average outflow over
+    # the last 500 seconds. Fig. 6 presents the average value, 1
+    # std-deviation from the average,
+
+    tested_flow_rates = 400
+    array_in = []
+    array_out = []
+    while tested_flow_rates < 2200:
+        exp = bottleneck_example(tested_flow_rates, 1000, render=True)
+        info = exp.run(10, 1000) #get more explanations for this
+        array_in += [tested_flow_rates]
+        tested_flow_rates += 1000
+        #outflow
+        array_out += [info['average_outflow']]
+    #visualize plot here
+    print(array_out)
+    print(array_in)
+
